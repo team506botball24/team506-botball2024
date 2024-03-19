25b3db1daf963be6fdeded1779491dda8714969f
team506botball24 <163337613+team506botball24@users.noreply.github.com>
2024-03-19 14:43:01 -0400

    Update period2.c
    Added timedServo function to slowly move servos

--- lego/period2.c
+++ lego/period2.c
@@ -3,29 +3,45 @@ int clawup=300;
 int clawdown=1050;
 int clawopen=1200;
 int clawclosed=1950;
+void timedServo(int port, int targetPos, int delay, int increment) {
+    int currentPos = get_servo_position(port);
+    if(targetPos - currentPos > 0) {
+        while(currentPos < targetPos) {
+            set_servo_position(port, currentPos + increment);
+            currentPos = get_servo_position(port);
+            msleep(delay);
+        }
+    } else {
+        while(currentPos > targetPos) {
+            set_servo_position(port, currentPos - increment);
+            currentPos = get_servo_position(port);
+            msleep(delay);
+        }
+    }
+}
 int main()
 {
     enable_servos();
-    set_servo_position(0,1050);
+    timedServo(0, clawdown, 30, 10);
     set_servo_position(1,1200);
     mav(0,1000);
     mav(3,1000);
-    msleep(8000);
-    mav(0,1000);
-    mav(3,-1000);
-    msleep(800);
+    msleep(8500);
+    mav(0,900);
+    mav(3,-900);
+    msleep(1200);
+    mav(0,-900);
+    mav(3,-900);
+    msleep(1500);
     ao();
-    set_servo_position(0,1000);
-    set_servo_position(0,800);
-    set_servo_position(0,600);
-    set_servo_position(0,400);
-    set_servo_position(0,300);
+    timedServo(0, clawup, 30, 10);
     msleep(500);
-    mav(0,500);
-    mav(3,500);
-    msleep(200);
+    mav(0,900);
+    mav(3,900);
+    msleep(900);
     ao();
     set_servo_position(1,1950);
+    msleep(1500);
     disable_servos();
     return 0;
 }
